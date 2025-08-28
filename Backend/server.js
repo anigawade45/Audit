571c6f2c6d09f5d03c0ee10b80e76d915def6509
anigawade45 <anigawade05@gmail.com>
2025-08-28 16:53:30 +0530

    change in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -41,7 +41,6 @@ app.use(
         },
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
         allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
-        credentials: true,
     })
 );
 
